664363dc1a52dc225bdacb16c3a59cbd374100a4
Mix4Pro <yaelseba@student.21-school.ru>
2025-10-21 21:34:35 +0500

    Worked on the first task

---
+++ src/main/java/lesson11/Main.java
@@ -0,0 +1,29 @@
+package lesson11;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<String> wordCollection = new LinkedList<>();
+
+
+        // Array to send not the copy , but the variable itself
+        boolean[] status = {true};
+
+        Thread publisher_td = new Thread(new Publisher(wordCollection, status));
+        Thread subscriber = new Thread(new Subscriber(wordCollection, status));
+
+        publisher_td.start();
+        subscriber.start();
+
+        try {
+            publisher_td.join();
+            subscriber.join();
+        } catch (InterruptedException e) {
+            System.out.println("The joining was interrupted");
+        }
+
+        System.out.println("Program exited");
+    }
+}
